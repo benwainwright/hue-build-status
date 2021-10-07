5bec2ea95daf33819a98d14879fd2335ae1d08ca
Ben Wainwright <ben.wainwright@cinch.co.uk>
2021-10-07 21:38:59 +0100

    Acquire initial password

--- client.spec.ts
+++ client.spec.ts
@@ -1,5 +1,39 @@
-describe("the hue client", () => {
+import { HueClient } from "./client";
+import nock from "nock";
 
-  it("generates a new username from the hue client if none is passed in", () => {
-  })
-})
+beforeEach(() => {
+  nock.disableNetConnect();
+});
+
+afterEach(() => {
+  nock.cleanAll();
+  nock.enableNetConnect();
+});
+
+describe("the hue client", () => {
+  describe("get", () => {
+    it("if no password is passed in, it gets one from the bridge and uses it in the request, then returns the response", async () => {
+      const testUsername = "foo-username";
+
+      const bridge = nock("http://123.123.123.123");
+
+      bridge.post("/api", '{"deviceType":"hue-build-status"}').reply(200, [
+        {
+          success: {
+            username: testUsername
+          }
+        }
+      ]);
+
+      const expectedResult = { foo: "bar" };
+
+      bridge.get(`/api/${testUsername}/foo-bar`).reply(200, expectedResult);
+
+      const client = new HueClient("123.123.123.123");
+
+      const actual = await client.get("/foo-bar");
+
+      expect(actual).toEqual(expectedResult);
+    });
+  });
+});
--- client.ts
+++ client.ts
@@ -1,4 +1,60 @@
+import axios, { AxiosResponse } from "axios";
+
+interface UsernameRequestType {
+  deviceType: string;
+}
+
+type UsernameResponseType = [
+  {
+    success: {
+      username: string;
+    };
+  }
+];
+
+type HttpMethod = "GET" | "POST";
+
 export class HueClient {
-  constructor(private ip: string, username?: string) {
+  constructor(private ip: string, private username?: string) {}
+
+  private async makeRequest<T, R>(method: HttpMethod, path: string, body?: T) {
+    const { data } = await axios.request<T, AxiosResponse<R>>({
+      method,
+      url: `http://${this.ip}/api${path}`,
+      data: body
+    });
+
+    return data;
+  }
+
+  private async makeAuthenticatedRequest<T = never, R = never>(
+    method: HttpMethod,
+    path: string,
+    body?: T
+  ) {
+    if (!this.username) {
+      const [
+        {
+          success: { username }
+        }
+      ] = await this.makeRequest<UsernameRequestType, UsernameResponseType>(
+        "POST",
+        "",
+        {
+          deviceType: "hue-build-status"
+        }
+      );
+
+      this.username = username;
+    }
+    return await this.makeRequest<T, R>(
+      method,
+      `/${this.username}${path}`,
+      body
+    );
+  }
+
+  async get(path: string) {
+    return await this.makeAuthenticatedRequest("GET", path);
   }
 }
